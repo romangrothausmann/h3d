e22fbc2fd13e54ecb0b43010e682ce564f39315e
Roman Grothausmann <romangrothausmann@users.noreply.github.com>
2016-06-06 13:56:05 +0200

    alternatives to top

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 
-## start logging CPU and RAM usage
+## start logging CPU and RAM usage without top
+# echo `date +%s` \
+#      `mpstat 2 1 | awk '$1 ~ /Average:/ && $12 ~ /[0-9.]+/ { print 100 - $12 }'` # http://stackoverflow.com/questions/9229333/how-to-get-overall-cpu-usage-e-g-57-on-linux \
+#      `free | awk '{if ($1 ~ /Mem:/) t=$2 } $2 ~ /buffers/ { print $4*100/t }'` # http://serverfault.com/questions/38065/how-to-get-memory-usage-with-vmstat
+## start logging CPU and RAM usage with top
 top -b -d 2 \
     | awk ' {if ($3 ~ /..:..:../) "date -d "$3" +%s" | getline T } # pipe time to date within awk: http://stackoverflow.com/questions/20646819/how-can-i-pass-variables-from-awk-to-a-shell-command#34420667 \
             {if ($1 ~ /%Cpu/) c=$8 } \
